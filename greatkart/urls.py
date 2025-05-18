53a094a99d0cdf9d6bed15d742541821c285b5cc
Wanderson Souza <wandersonsouza392@gmail.com>
2025-05-18 19:06:29 -0300

    Create a Cart Model

--- greatkart/urls.py
+++ greatkart/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('store/', include('store.urls')),
+    path('cart/', include('carts.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

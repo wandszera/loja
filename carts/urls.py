eb5a0eafbb2a36ca56ca1f083f2048225d68b32a
Wanderson Souza <wandersonsouza392@gmail.com>
2025-05-19 21:42:11 -0300

    Cart Item

--- carts/urls.py
+++ carts/urls.py
@@ -3,12 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.cart, name='cart'),
-    # path('cart/', CartView.as_view(), name='cart'),
-    # path('cart/add/<int:product_id>/', AddToCartView.as_view(), name='add_to_cart'),
-    # path('cart/remove/<int:product_id>/', RemoveFromCartView.as_view(), name='remove_from_cart'),
-    # path('cart/update/<int:product_id>/', UpdateCartView.as_view(), name='update_cart'),
-    # path('cart/checkout/', CheckoutView.as_view(), name='checkout'),
-    # path('cart/checkout/success/', CheckoutSuccessView.as_view(), name='checkout_success'),
-    # path('cart/checkout/failure/', CheckoutFailureView.as_view(), name='checkout_failure'),
-    # path('cart/checkout/payment/', PaymentView.as_view(), name='payment'),
+    path('add_cart/<int:product_id>/', views.add_cart, name='add_cart'),
 ]
